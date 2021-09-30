cf1f329d6e88394abc9ae9e63abc57153453be3c
Chi-Han Wu <ddr5688.cs07@nctu.edu.tw>
2021-09-30 23:33:18 +0800

    Update curl-test.sh

--- Final/curl-test.sh
+++ Final/curl-test.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 # change the "to" token to match with the site you want to send the notification to
+# "registration_ids" token with comma & "" if there is more than one web
 # token can be found from checking console log from the website, can be found using inspect element
-# fAGraVthxb2DIoLQt9inPK:APA91bESAR8djlYJuCq1VDxpY9mkW0Pzud3ZZKTqa9L1pyZ5D4aUSbCwOkTZpK-nOjCkdyQz0Zl4DugjtI9G4SnW2xExyihf7vxn0lg8KoPlsjRz_K53QJ-vzCCbfY-XwAe0gEBjgqQP
 
 data='{ "data":{ "notification": {"title": "1001    1004    1002", "body": "test"}}, "to": "dbvFEtuJfHuujOsSklC2jq:APA91bFX_aEn67m3OveNxw8-ESS_Uhq0eMgVhZP3U6MBiAxayQ3ayAHyagyYt1Iyh3-kc5tXED06TIRVDg6f6_tD54DQm5se8PI-GBi4fKaerrYX0Shkl1dNYOtfBy94DZPPOmcVtKuZ"}';
 
